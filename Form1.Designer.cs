edc6688073abbaceabd531da74112eaafc8c9722
Администратор <@MININT-LRTEDND>
2024-06-09 13:21:37 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(84, 210);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(234, 27);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(412, 210);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(269, 27);
+            dateTimePicker2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(160, 115);
+            button1.Name = "button1";
+            button1.Size = new Size(183, 33);
+            button1.TabIndex = 2;
+            button1.Text = "Вычислить Разницу";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(394, 115);
+            button2.Name = "button2";
+            button2.Size = new Size(196, 33);
+            button2.TabIndex = 3;
+            button2.Text = "Добавить дни";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_click;
+            button2.Click += button2_click;
+        }
+
+        public void button1_click(object sender, EventArgs e)
+        {
+            TimeSpan ts = dateTimePicker1.Value - dateTimePicker2.Value;
+
+            Label label = new Label();
+            label.Text = "Разница во времени: " + ts.ToString();
+            label.AutoSize = true;
+
+     
+            label.Location = new Point(20, 80); 
+            Controls.Add(label);
+        }
+
+        public void button2_click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Введите количество дней для добавления");
+
+            if (int.TryParse(input, out int daysToAdd))
+            {
+                dateTimePicker1.Value = dateTimePicker1.Value.AddDays(daysToAdd);
+                MessageBox.Show($"Результат: {dateTimePicker1.Value.ToShortDateString()}");
+            }
+            else
+            {
+                MessageBox.Show("Некорректное значение!");
+            }
+        }
+    }
+}
